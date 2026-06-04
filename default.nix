6a844e58ba9263668d8ae0374acce2ca096a15b7
chekist32 <vladkra@proton.me>
2026-06-04 19:17:41 +0200

    i

--- default.nix
+++ default.nix
@@ -1,15 +1,21 @@
-let
-  pkgs = import <nixpkgs> { };
-in
-pkgs.stdenv.mkDerivation (finalAttrs: {
-  pname = "hello";
-  version = "2.12";
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  packages = with pkgs; [
+    (python3.withPackages (
+      ps: with ps; [
+        jupyter
+        numpy
+        matplotlib
+      ]
+    ))
+  ];
 
-  src = pkgs.fetchurl {
-    url = "https://ftp.gnu.org/gnu/hello/hello-${finalAttrs.version}.tar.gz";
-    sha256 = "1ayhp9v4m4rdhjmnl2bq3cibrbqqkgjbl3s7yk2nhlh8vj3ay16g";
-  };
-
-  doInstallCheck = false;
-  doCheck = false;
-})
+  shellHook = ''
+    echo "🐍 Python: $(python --version)"
+    echo "📓 Jupyter: $(jupyter --version | head -1)"
+    echo ""
+    echo "run: jupyter notebook demo.ipynb"
+  '';
+}
