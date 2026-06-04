f90090f157bf2401a4e18c8611750b687209f5cf
chekist32 <vladkra@proton.me>
2026-06-04 20:15:11 +0200

    i

--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ?
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8c50a710ddca43d7a530fb805ad55bde8d0141c5.tar.gz")
+      { },
 }:
 pkgs.mkShell {
   packages = with pkgs; [
