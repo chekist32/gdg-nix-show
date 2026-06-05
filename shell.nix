0bf6cfc1fe30e8a427cacfda9a2366a51b770f84
chekist32 <vladkra@proton.me>
2026-06-05 08:12:06 +0200

    i

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 {
-  pkgs ?
-    import
-      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8c50a710ddca43d7a530fb805ad55bde8d0141c5.tar.gz")
-      { },
+  pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/8c50a710ddca43d7a530fb805ad55bde8d0141c5.tar.gz";
+    sha256 = "0am8xx09fx5yf2p0wb001v0jx1g5hrfb76h4r37xph378jgk7pcr";
+  }) { },
 }:
 pkgs.mkShell {
   packages = with pkgs; [
